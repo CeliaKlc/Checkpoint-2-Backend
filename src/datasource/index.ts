e2c271b25c55452e3bbda537665d7744b9557e97
Celia Kleczynski <Celia.kleczynski@hotmail.com>
2024-12-02 11:52:40 +0100

    creation of Country, database and resolvers

---
+++ src/datasource/index.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import { Country } from "../entities/Country";
+
+export const dataSource = new DataSource({
+  type: "sqlite",
+  database: "./db/database.sqlite",
+  entities: ["src/entities/*.ts"],
+  synchronize: true,
+  logging: "all",
+});
+
+export async function cleanDB() {
+  await dataSource.manager.clear(Country);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello");
+import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import { buildSchema } from "type-graphql";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { cleanDB, dataSource } from "./datasource";
+import { CountryMutations } from "./resolvers/CountryMutation";
+import { CountryQueries } from "./resolvers/CountryQueries";
+
+const port = 4000;
+
+async function startAppolloServer() {
+  const schema = await buildSchema({
+    resolvers: [CountryQueries, CountryMutations],
+  });
+  const server = new ApolloServer({ schema });
+  await dataSource.initialize();
+  await startStandaloneServer(server, { listen: { port } });
+  await cleanDB();
+
+  console.log(`Le serveur a démarré au port : ${port} !`);
+}
+
+startAppolloServer();
